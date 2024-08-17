0643ced5baadffcbf3e596189cac068c11dce05a
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 19:14:14 -0300

    user routes: get by username

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const UserContoller = require("../controllers/userController");
 
 user_routes.post("/", UserContoller.create);
 user_routes.get("/", UserContoller.getAll);
+user_routes.get("/:username", UserContoller.getByUsername);
 
 module.exports = user_routes;
