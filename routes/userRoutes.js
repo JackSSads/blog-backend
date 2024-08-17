f9be6cc4348df96e0259f8fc3fd24b00cacb6d08
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 19:24:40 -0300

    use routes: update by username

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const UserContoller = require("../controllers/userController");
 user_routes.post("/", UserContoller.create);
 user_routes.get("/", UserContoller.getAll);
 user_routes.get("/:username", UserContoller.getByUsername);
+user_routes.put("/:username", UserContoller.updateByUsername);
 
 module.exports = user_routes;
