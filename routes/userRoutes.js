c022625377189c122b48ab50586e6e814b6ac5b2
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 19:04:59 -0300

    user routes: get/all

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,6 @@ const user_routes = require("express").Router();
 const UserContoller = require("../controllers/userController");
 
 user_routes.post("/", UserContoller.create);
+user_routes.get("/", UserContoller.getAll);
 
 module.exports = user_routes;
