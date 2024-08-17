85b5065833f0359665751cf9779eda4bbe7bc2cf
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 18:08:02 -0300

    export auth_middlewere

--- middleweres/index.js
+++ middleweres/index.js
@@ -1,9 +1,11 @@
 const logger = require("./logger");
 const conf_cors = require("./cors");
 const conf_session = require("./sessions");
+const auth_middlewere = require("./authMiddlewere");
 
 module.exports = {
     logger,
     conf_cors,
-    conf_session
+    conf_session,
+    auth_middlewere
 };
