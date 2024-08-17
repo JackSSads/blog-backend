a50e65f2cb13328bda2bc5d01dd9a351f9906eb1
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 17:49:18 -0300

    model post

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db/connection');
+const User = require('./User');
+
+const Post = db.define('Post', {
+    post_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    published_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+}, {
+    timestamps: true,
+    tableName: "posts"
+});
+
+
+Post.belongsTo(User, { foreignKey: 'user_id' });
+User.hasMany(Post, { foreignKey: 'user_id' });
+
+module.exports = Post;
