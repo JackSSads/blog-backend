f0b40c5a1212272c0879f4211271573dbeca4fdb
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 21:47:53 -0300

    user routes: delete by id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ user_routes.post("/", UserContoller.create);
 user_routes.get("/", UserContoller.getAll);
 user_routes.get("/:user_id", UserContoller.getById);
 user_routes.put("/:user_id", UserContoller.updateById);
+user_routes.delete("/:user_id", UserContoller.deleteById);
 
 module.exports = user_routes;
