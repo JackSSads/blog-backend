2734a8a5e45453e2512b40755231c6f3acd7bd6a
JackSSads <fotosjackson2018@hotmail.com>
2024-08-17 22:46:23 -0300

    exporting category routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const auth_router = require("./authRouter");
 const user_routes = require("./userRoutes");
+const category_routes = require("./categoryRoutes");
 
 module.exports = { 
     auth_router,
-    user_routes
+    user_routes,
+    category_routes
 };
