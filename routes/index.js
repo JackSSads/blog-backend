a1ff688a1ffc3bd1d54ab09d5faf620e49a60afc
JackSSads <contatojacksonsouza@hotmail.com>
2024-09-01 17:01:08 -0300

    exportando rotas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const auth_router = require("./authRouter");
+
 const user_routes = require("./userRoutes");
 const category_routes = require("./categoryRoutes");
+const tag_routes = require("./tagRoutes");
+const comments_routes = require("./commentsRoutes");
+const post_routes = require("./postRoutes");
 
-module.exports = { 
+const post_category_routes = require("./postCategoryRoutes");
+const post_tag_routes = require("./postTagRoutes");
+
+module.exports = {
     auth_router,
     user_routes,
-    category_routes
+    category_routes,
+    tag_routes,
+    comments_routes,
+    post_routes,
+    post_category_routes,
+    post_tag_routes
 };
