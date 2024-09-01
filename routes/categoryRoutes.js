93893e1141206f6bb6a82a453ae315f5583be4be
JackSSads <contatojacksonsouza@hotmail.com>
2024-09-01 16:48:49 -0300

    atualização

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,8 +3,8 @@ const CategoryController = require("../controllers/CategoryController");
 
 category_routes.post("/", CategoryController.create);
 category_routes.get("/", CategoryController.getAll);
-category_routes.get("/", CategoryController.getById);
-category_routes.put("/", CategoryController.updateById);
-category_routes.delete("/", CategoryController.deleteById);
+category_routes.get("/:category_id", CategoryController.getById);
+category_routes.put("/:category_id", CategoryController.updateById);
+category_routes.delete("/:category_id", CategoryController.deleteById);
 
 module.exports = category_routes;
